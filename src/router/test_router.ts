3fcf2f8500d83b8d20f4199d01095e85028d47fb
najongjine <najongjin3@hotmail.com>
2026-01-15 14:38:32 +0900

    .

--- src/router/test_router.ts
+++ src/router/test_router.ts
@@ -1,6 +1,17 @@
 import { Hono } from "hono";
-import type { HonoEnv } from "../types/types.js";
+import type { HonoEnv, ResultType } from "../types/types.js";
 
 const router = new Hono<HonoEnv>();
+
+router.get("/db_select_test", async (c) => {
+  let result: ResultType = { success: true };
+  try {
+    return c.json(result);
+  } catch (error: any) {
+    result.success = false;
+    result.msg = `!error. ${error?.message}`;
+    return c.json(result);
+  }
+});
 
 export default router;
