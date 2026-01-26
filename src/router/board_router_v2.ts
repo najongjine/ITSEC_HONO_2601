77d6138098c01ef53f303ff8fd965492810cc271
najongjine <najongjin3@hotmail.com>
2026-01-26 14:42:08 +0900

    .

--- src/router/board_router_v2.ts
+++ src/router/board_router_v2.ts
@@ -169,4 +169,56 @@ router.post("/upsert", async (c) => {
   }
 });
 
+router.post("/upsert", async (c) => {
+  let result: ResultType = { success: true };
+  const db = c.var.db;
+  try {
+    const authHeader = c.req.header("Authorization");
+    if (!authHeader || !authHeader.startsWith("Bearer ")) {
+      result.success = false;
+      result.msg = "!error. 토큰이 유효하지 않습니다(Header).";
+      return c.json(result);
+    }
+
+    const token = authHeader.split(" ")[1];
+    let userData: any = verifyToken(token);
+    if (!userData) {
+      result.success = false;
+      result.msg = "!error. 토큰이 유효하지 않습니다(verify).";
+      return c.json(result);
+    }
+
+    const body = await c?.req?.json();
+    let id = Number(body?.id ?? 0);
+
+    let _data: any = await db.query(
+      `
+        SELECT
+        b.id
+        ,b.user_id as "userId"
+        ,b.title
+        ,b.content
+        ,b.created_dt as "createdDt"
+        ,b.updated_dt as "updatedDt"
+        ,b.html as "htmlContent"
+        ,b.json as "jsonContent"
+        ,u.username
+        FROM t_board as b
+        LEFT JOIN t_user as u ON u.id=b.user_id
+        WHERE b.id = $1
+        ORDER BY b.id DESC
+        
+        `,
+      [id],
+    );
+    _data = _data.rows[0] || {};
+
+    return c.json(result);
+  } catch (error: any) {
+    result.success = false;
+    result.msg = `!error. ${error?.message}`;
+    return c.json(result);
+  }
+});
+
 export default router;
