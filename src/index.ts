d875edc19f20aa62029c7ad29ff0a5a1508b8908
najongjine <najongjin3@hotmail.com>
2026-03-04 14:06:07 +0900

    .

--- src/index.ts
+++ src/index.ts
@@ -40,6 +40,9 @@ app.route("/api/board", boardRouter);
 import boardRouterV2 from "./router/board_router_v2.js";
 app.route("/api/board_v2", boardRouterV2);
 
+import embeddingRouter from "./router/embedding_router.js";
+app.route("/api/embedding", embeddingRouter);
+
 serve(
   {
     fetch: app.fetch,
